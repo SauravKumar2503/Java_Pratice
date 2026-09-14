e2cb9d8a3d7b016bec6a680e5ca7470a3df8419d
Saurav Kumar <skauumraarv2003@gmail.com>
2026-09-14 13:38:58 +0530

    N natural number

---
+++ FirstNnaturalNum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class FirstNnaturalNum {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the term: ");
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            System.out.print(i + " ");
+        }
+        sc.close();
+    }
+}
