37578ce883a7e21eb907177e1434d7ffc82e59d1
Saurav Kumar <skauumraarv2003@gmail.com>
2026-09-14 18:03:21 +0530

    Power of a number

---
+++ PowerOfNum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class PowerOfNum {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the base number: ");
+        int base = sc.nextInt();
+        System.out.println("Enter the exponent: ");
+        int exponent = sc.nextInt();
+        int result = (int)Math.pow(base,exponent);
+        System.out.println(result);
+        sc.close();
+    }
+}
