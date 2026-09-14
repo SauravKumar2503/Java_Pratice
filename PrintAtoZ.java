72df52b50d433bcafaf30b50363f0d280279d958
Saurav Kumar <skauumraarv2003@gmail.com>
2026-09-14 17:42:05 +0530

    A to Z character

---
+++ PrintAtoZ.java
@@ -0,0 +1,9 @@
+
+public class PrintAtoZ {
+    public static void main(String[] args){
+        for(char ch = 'A'; ch <= 'Z';ch++)
+        {
+            System.out.print(ch + " ");       
+        }
+    }
+}
