f42007081ced248b3a944c9c970f08de78152f21
Saurav Kumar <skauumraarv2003@gmail.com>
2026-09-16 12:42:53 +0530

    Swap two number without using the third Variable

---
+++ SwapTwoNumWithoutThird.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class SwapTwoNumWithoutThird {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two number a and b: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        a = a + b;
+        b = a-b;
+        a = a-b;
+        System.out.println("After swapping: a = " + a + ", b = " + b);
+    }
+}
