0512f49befb6e25e54a7b8ee3458a0dba5cdc880
Saurav Kumar <skauumraarv2003@gmail.com>
2026-09-18 12:13:36 +0530

    Pattern 2

---
+++ Pattern/Pattern2.java
@@ -0,0 +1,21 @@
+package Pattern;
+
+import java.util.*; 
+
+public class Pattern2 {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = sc.nextInt();
+        for(int row = 1;row<=n;row++)
+        {
+            for(int col = 1;col <= row;col++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+            sc.close();
+        }
+    }    
+}
