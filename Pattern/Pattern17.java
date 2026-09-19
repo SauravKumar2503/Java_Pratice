04dcaedaa8d3104913c7ce034b9be004033b36e9
Saurav Kumar <skauumraarv2003@gmail.com>
2026-09-19 12:11:32 +0530

    Alpha-Hill Pattern

---
+++ Pattern/Pattern17.java
@@ -0,0 +1,36 @@
+package Pattern;
+
+public class Pattern17 {
+    public static void main(String[] args)
+    {
+        int N =5;
+        for (int i = 0; i < N; i++) {
+
+            // Print leading spaces
+            for (int j = 0; j < N - i - 1; j++) {
+                System.out.print(" ");
+            }
+
+            // Initialize character to start from 'A'
+            char ch = 'A';
+            int breakpoint = (2 * i + 1) / 2;
+
+            // Print characters in row
+            for (int j = 1; j <= 2 * i + 1; j++) {
+                System.out.print(ch);
+
+                // Increment or decrement character
+                if (j <= breakpoint) ch++;
+                else ch--;
+            }
+
+            // Print trailing spaces
+            for (int j = 0; j < N - i - 1; j++) {
+                System.out.print(" ");
+            }
+
+            // Newline after each row
+            System.out.println();
+        }
+    }
+}
